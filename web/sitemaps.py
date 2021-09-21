e455368a09e183ae8520203152d7a194895325e3
velikanovv <andreyvelikanovv@gmail.com>
2021-09-21 16:25:22 +0300

    tattoogen v1.4

--- web/sitemaps.py
+++ web/sitemaps.py
@@ -1,9 +1,34 @@
 from django.contrib.sitemaps import Sitemap
 from core.models import *
 
+class MainSitemap(Sitemap):
+
+    location = "/"
+    changefreq = "weekly"
+    priority = 1
+
+    def items(self):
+      return [self]
+
+class MainBlogSitemap(Sitemap):
+
+    location = "blog/"
+    changefreq = "weekly"
+    priority = 1
+
+    def items(self):
+      return [self]
+
+class PostCategorySitemap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.7
+
+    def items(self):
+        return PostCategory.objects.all()
+
 class PostSitemap(Sitemap):
     changefreq = 'weekly'
-    priority = 0.8
+    priority = 0.7
 
     def items(self):
         return Post.objects.all()
@@ -12,15 +37,15 @@ class PostSitemap(Sitemap):
         return obj.date
 
 class SketchSitemap(Sitemap):
-    changefreq = 'weekly'
-    priority = 0.9
+    changefreq = 'never'
+    priority = 0.8
 
     def items(self):
         return Sketch.objects.filter(is_main=False).all()
 
 class SketchMainSitemap(Sitemap):
-    changefreq = 'weekly'
-    priority = 1
+    changefreq = 'never'
+    priority = 0.9
 
     def items(self):
         return Sketch.objects.filter(is_main=True).all()
